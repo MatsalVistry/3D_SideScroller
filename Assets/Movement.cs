fa853e28cc8266eb10c64a1d81709618ad15bcde
Vatsal Mistry <vatsalmistry4796@gmail.com>
2019-10-16 19:52:10 -0500

    Complete Project

---
+++ Assets/Movement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+    public double speed;
+    public bool jumping;
+    private double i = 0.0;
+    public float jumpspeed = 0.1f;
+    public Vector3 initialvelocity;
+    
+     Rigidbody rb ;
+    public float distToGround = 0.5f;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (i >= 1.0)
+        {
+            Vector3 initial = new Vector3(0, jumpspeed, 0);
+            rb.velocity = rb.velocity - initial;
+          
+        }
+        if (Input.GetKey(KeyCode.Space) & IsGrounded()){
+            // rb.AddForce(Vector3.up * 3);
+         
+            Vector3 jumpVelocity = new Vector3(0, jumpspeed, 0);
+          
+            rb.velocity = rb.velocity + jumpVelocity;
+            
+            jumping = true;
+        }
+
+        if (Input.GetKey("space") & IsGrounded())
+        {
+            print("dfa");
+        }
+      
+            transform.Translate(0, 0, Time.deltaTime +0.03f);
+
+    }  
+  bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, distToGround);  
+            }
+}
---
+++ Assets/Movementscript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movementscript : MonoBehaviour
+{
+    public bool onGround;
+    private bool onObs;
+    public Rigidbody rb2d;
+    public float gravityScale = -10.0f;
+    public static float globalGravity = -10f;
+    Vector3 tempPos;
+    // Use this for initialization
+    void Start()
+    {
+        onGround = true;
+        rb2d.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    { tempPos = transform.position;
+            tempPos.z += .1f;
+            transform.position = tempPos;
+            
+        
+        if (onGround || onObs)
+        {
+         
+            tempPos = transform.position;
+            tempPos.z += .2f;
+            transform.position = tempPos;
+            
+            if (Input.GetButtonDown("Jump"))
+            {
+
+                rb2d.velocity = new Vector3(0f, 10f, 0f);
+                Vector3 gravity = globalGravity * gravityScale * Vector3.up;
+                rb2d.AddForce(gravity, ForceMode.Acceleration);
+                onGround = false;
+                onObs = false;
+                
+            }
+        }
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("groun"))
+        {
+            onGround = true;
+            
+        }
+        if (other.gameObject.CompareTag("obstacle"))
+        {
+            onObs = true;
+        }
+        if(other.gameObject.CompareTag("Finish"))
+        {
+            transform.position = new Vector3(1.94f, 5.06f, -56.05f);
+        }
+    }
+
+
+}
